55f307cb0ecd647b779bb8f7dcaf34dc776a445d
Ndimah Mike <jospainevans@gmail.com>
2021-08-18 12:07:36 +0100

    added groupping anagram feature

---
+++ test/anagrams.test.js
@@ -0,0 +1,15 @@
+var assert = require('chai').assert;
+var { groupAnagrams } = require('../src/coding_interviews_algorithms/anagrams');
+
+describe('group anagrams elements in list', function () {
+    var inputs, expected;
+    before(function () {
+        inputs = ['tab', 'bat', 'taste', 'state', 'teats', 'ates', 'eats', 'teas', 'seat'];
+        expected = [['tab', 'bat',], ['taste', 'state', 'teats'], ['ates', 'eats', 'teas', 'seat']];
+    });
+    
+    it('should group the inputs anagrams elements', function () {
+        result = groupAnagrams(inputs);
+        assert.deepEqual(result,expected);
+    });
+});

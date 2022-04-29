05f88f9e7cb0eb96cfc00ac492627c78c4071855
Ndimah Mike <jospainevans@gmail.com>
2022-04-29 19:34:29 +0100

    feat: implement the unbounded knapsack problems

--- src/coding_interviews_algorithms/dynamic_programming_problems.js
+++ src/coding_interviews_algorithms/dynamic_programming_problems.js
@@ -1,106 +1,176 @@
-
-function maximumProfitFittingCapacity(profits, weights, capacity) {
-    var n = profits.length, dp = Array(capacity + 1).fill(0)
-    if(capacity<=0||n===0||weights.length !== n) return 0
-    for (let i = 0; i <= capacity; i++){
-        if(weights[0]<=i) dp[i]=profits[0]
+function maxProfitFittingCapacityHavingDistinctItems(
+  profits,
+  weights,
+  capacity
+) {
+  var n = profits.length,
+    dp = Array(capacity + 1).fill(0);
+  if (capacity <= 0 || n === 0 || weights.length !== n) return 0;
+  for (let i = 0; i <= capacity; i++) {
+    if (weights[0] <= i) dp[i] = profits[0];
+  }
+  for (let i = 1; i < n; i++) {
+    for (let c = capacity; c >= 0; c--) {
+      let profit2 = 0,
+        profit1 = 0;
+      if (weights[i] <= c) profit1 = profits[i] + dp[c - weights[i]];
+      profit2 = dp[c];
+      dp[c] = Math.max(profit1, profit2);
     }
-    for (let i = 1; i < n; i++){
-        for (let c = capacity; c >= 0; c--){
-            let profit2=0,profit1=0
-            if (weights[i] <= c) profit1 = profits[i]+dp[c-weights[i]]
-            profit2 = dp[c]
-            dp[c]=Math.max(profit1,profit2)
-        }
-    }
-    return dp[capacity]
+  }
+  return dp[capacity];
 }
-
-function hasTwoPartitionsOfEqualSum(array) {
-    let sum = array.reduce((prev, current) => current + prev, 0), length = array.length
-    if(sum%2!==0) return false
-    sum /=2
-    dp =  Array(sum+1).fill(false)
-    dp[0] = true
-    for (let i = 1; i <= sum; i++) dp[i] = array[0] === i
-    for (let i = 1; i < length; i++){
-        for (let j = sum; j>=0; j--){
-            if (!dp[j] && j >= array[i]) dp[j] = dp[j-array[i]]
-        }
+function maxProfitFittingCapacityWithAnyItems(profits, weights, capacity) {
+  var n = profits.length,
+    dp = Array(capacity + 1).fill(0);
+  for (let i = 1; i < dp.length; i++) {
+    if (weights[0] > i) dp[i] = 0;
+    else dp[i] = profits[0] + dp[i - weights[0]];
+  }
+  for (let i = 1; i < n; i++) {
+    for (let c = 1; c <= capacity; c++) {
+      if (weights[i] <= c)
+        dp[c] = Math.max(dp[c], profits[i] + dp[c - weights[i]]);
     }
-    return dp[sum]
+  }
+  return dp[capacity];
+}
+function allPossibleChangesOf(amount, denominations) {
+  var n = denominations.length,
+    dp = Array(amount + 1).fill(0);
+  for (let i = 1; i < dp.length; i++) if (i % denominations[0] === 0) dp[i] = 1;
+  for (let i = 1; i < n; i++) {
+    let currentDenomination = denominations[i];
+    for (let c = 1; c <= amount; c++) {
+      if (currentDenomination === c) dp[c] = dp[c] + 1;
+      else if (currentDenomination < c)
+        dp[c] = dp[c] + dp[c - denominations[i]];
+    }
+  }
+  return dp[amount];
+}
+function minDenominationsToChange(amount, denominations) {
+  var n = denominations.length,
+    dp = Array(amount + 1).fill(Number.POSITIVE_INFINITY);
+  for (let i = 0; i < dp.length; i++) dp[i] = Math.floor(i / denominations[0]);
+  for (let i = 1; i < n; i++) {
+    let currentDenomination = denominations[i];
+    for (let c = 1; c <= amount; c++) {
+      if (currentDenomination <= c) {
+        let currentChanges =
+          Math.floor(c / currentDenomination) + dp[c % currentDenomination];
+        if (dp[c] === 0) dp[c] = currentChanges;
+        else dp[c] = Math.min(dp[c], currentChanges);
+      }
+    }
+  }
+  return dp[amount];
+}
+function maximumRibbonCuts(ribbonLength, cutLengths) {
+  var cuts = cutLengths.length,
+    dp = Array(ribbonLength + 1).fill(0);
+  for (let i = 0; i < dp.length; i++) {
+    if (i % cutLengths[0] === 0) dp[i] = i / cutLengths[0];
+  }
+  for (let i = 1; i < cuts; i++) {
+    let currentCut = cutLengths[i];
+    for (let c = 1; c <= ribbonLength; c++) {
+      if (currentCut <= c) dp[c] = Math.max(1 + dp[c - currentCut], dp[c]);
+    }
+  }
+  return dp[ribbonLength];
+}
+function hasTwoPartitionsOfEqualSum(array) {
+  let sum = array.reduce((prev, current) => current + prev, 0),
+    length = array.length;
+  if (sum % 2 !== 0) return false;
+  sum /= 2;
+  dp = Array(sum + 1).fill(false);
+  dp[0] = true;
+  for (let i = 1; i <= sum; i++) dp[i] = array[0] === i;
+  for (let i = 1; i < length; i++) {
+    for (let j = sum; j >= 0; j--) {
+      if (!dp[j] && j >= array[i]) dp[j] = dp[j - array[i]];
+    }
+  }
+  return dp[sum];
 }
 function hasSubsetWithSum(array, sum) {
-    if (array.length === 0) return false
-    let length = array.length
-    dp = Array(sum + 1).fill(false)
-    dp[0]=true
-    for (let i = 1; i <= sum;i++) dp[i] = array[0]===i
-    for (let i = 1; i < length; i++){
-        for (let j = sum; j >=0; j--){
-            if (!dp[j] && j>=array[i]) dp[j] = dp[j-array[i]]
-        }
+  if (array.length === 0) return false;
+  let length = array.length;
+  dp = Array(sum + 1).fill(false);
+  dp[0] = true;
+  for (let i = 1; i <= sum; i++) dp[i] = array[0] === i;
+  for (let i = 1; i < length; i++) {
+    for (let j = sum; j >= 0; j--) {
+      if (!dp[j] && j >= array[i]) dp[j] = dp[j - array[i]];
     }
-    return dp[sum]
+  }
+  return dp[sum];
 }
-function minimumDifferenceBetweenTwoSubSetSum(array){
-    let sum = array.reduce((prev, current) => prev + current, 0),
-    halfSum = Math.floor(sum/2)
-    dp = Array(halfSum + 1).fill(false)
-    dp[0] = true
-    for (let i = 1; i < halfSum;i++) dp[i] = array[0] === i
-    for (let i = 1; i < array.length; i++){
-        for (let j = halfSum; j >= 0; j--){
-            if(!dp[j] && j>=array[i]) dp[j]=dp[j-array[i]]
-        }
+function minimumDifferenceBetweenTwoSubSetSum(array) {
+  let sum = array.reduce((prev, current) => prev + current, 0),
+    halfSum = Math.floor(sum / 2);
+  dp = Array(halfSum + 1).fill(false);
+  dp[0] = true;
+  for (let i = 1; i < halfSum; i++) dp[i] = array[0] === i;
+  for (let i = 1; i < array.length; i++) {
+    for (let j = halfSum; j >= 0; j--) {
+      if (!dp[j] && j >= array[i]) dp[j] = dp[j - array[i]];
     }
-    let index = halfSum
-    while (!dp[index]) {
-        index--
-    }
-    return Math.abs(2*index-sum)
+  }
+  let index = halfSum;
+  while (!dp[index]) {
+    index--;
+  }
+  return Math.abs(2 * index - sum);
 }
 function countOfSubsetWithSum(array, sum) {
-    var dp = Array(sum + 1).fill(0), arraySum = array.reduce((prev, current) => prev + current, 0)
-    if(sum>arraySum) return 0
-    dp[0] = 1
-    for (let i = 1; i <= sum; i++) dp[i] = array[0] === i ? 1 : 0
-    for (let i = 1; i < array.length; i++){
-        for (let j = sum; j  >=0; j--){
-            if (j >= array[i]) dp[j] = dp[j] + dp[j - array[i]]
-        }
+  var dp = Array(sum + 1).fill(0),
+    arraySum = array.reduce((prev, current) => prev + current, 0);
+  if (sum > arraySum) return 0;
+  dp[0] = 1;
+  for (let i = 1; i <= sum; i++) dp[i] = array[0] === i ? 1 : 0;
+  for (let i = 1; i < array.length; i++) {
+    for (let j = sum; j >= 0; j--) {
+      if (j >= array[i]) dp[j] = dp[j] + dp[j - array[i]];
     }
-    return dp[sum]
+  }
+  return dp[sum];
 }
 function numberOfSymbolsCombinationToHaveTheSum(array, sum) {
-    var arraySum = array.reduce((prev, current) => prev + current, 0)
-    if (sum > arraySum || (sum + arraySum) % 2 === 1) return 0;
-    sum = (sum + arraySum)/2
-    let dp = Array(sum + 1).fill(0)
-    dp[0] = 1
-    for (let i = 1; i <= sum; i++) dp[i] = array[0] === i ? 1 : 0
-    for (let i = 1; i < array.length; i++){
-        for (let j = sum; j >= 0; j--){
-            if(j>=array[i]) dp[j] += dp[j-array[i]]
-        }
+  var arraySum = array.reduce((prev, current) => prev + current, 0);
+  if (sum > arraySum || (sum + arraySum) % 2 === 1) return 0;
+  sum = (sum + arraySum) / 2;
+  let dp = Array(sum + 1).fill(0);
+  dp[0] = 1;
+  for (let i = 1; i <= sum; i++) dp[i] = array[0] === i ? 1 : 0;
+  for (let i = 1; i < array.length; i++) {
+    for (let j = sum; j >= 0; j--) {
+      if (j >= array[i]) dp[j] += dp[j - array[i]];
     }
-    return dp[sum]
+  }
+  return dp[sum];
 }
 Array.prototype.hasTwoPartitionsOfEqualSum = function () {
-    return hasTwoPartitionsOfEqualSum(this)
-}
+  return hasTwoPartitionsOfEqualSum(this);
+};
 Array.prototype.hasSubsetWithSum = function (sum) {
-    return hasSubsetWithSum(this,sum)
-}
+  return hasSubsetWithSum(this, sum);
+};
 Array.prototype.minimumDifferenceBetweenTwoSubSetSum = function () {
-    return minimumDifferenceBetweenTwoSubSetSum(this)
-}
+  return minimumDifferenceBetweenTwoSubSetSum(this);
+};
 Array.prototype.countOfSubsetWithSum = function (sum) {
-    return countOfSubsetWithSum(this,sum)
-}
+  return countOfSubsetWithSum(this, sum);
+};
 Array.prototype.numberOfSymbolsCombinationToHaveTheSum = function (sum) {
-    return numberOfSymbolsCombinationToHaveTheSum(this,sum)
-}
+  return numberOfSymbolsCombinationToHaveTheSum(this, sum);
+};
 module.exports = {
-    maximumProfitFittingCapacity
-}
+  maxProfitFittingCapacityHavingDistinctItems,
+  maxProfitFittingCapacityWithAnyItems,
+  allPossibleChangesOf,
+  minDenominationsToChange,
+  maximumRibbonCuts,
+};
--- test/dynamic_programming_problems.test.js
+++ test/dynamic_programming_problems.test.js
@@ -1,51 +1,93 @@
-const { assert } = require('chai');
-const { maximumProfitFittingCapacity } = require("../src/coding_interviews_algorithms/dynamic_programming_problems")
+const { assert } = require("chai");
+const {
+  maxProfitFittingCapacityHavingDistinctItems,
+  maxProfitFittingCapacityWithAnyItems,
+  allPossibleChangesOf,
+  minDenominationsToChange,
+  maximumRibbonCuts,
+} = require("../src/coding_interviews_algorithms/dynamic_programming_problems");
 describe("knapsack problems", function () {
-    it("should return the maximum profit for items fitting in a given capacity", function () {
-        var profits = [1,6,10,16],weights=[1,2,3,5]
-        assert.equal(maximumProfitFittingCapacity(profits,weights,7),22)
-        assert.equal(maximumProfitFittingCapacity(profits,weights,6),17)
-    })
-})
+  it("should return the maximum profit for items fitting in a given capacity with distinct items", function () {
+    var profits = [1, 6, 10, 16],
+      weights = [1, 2, 3, 5];
+    assert.equal(
+      maxProfitFittingCapacityHavingDistinctItems(profits, weights, 7),
+      22
+    );
+    assert.equal(
+      maxProfitFittingCapacityHavingDistinctItems(profits, weights, 6),
+      17
+    );
+  });
+  it("should return the maximum profit for items fitting a given capacity with any items", function () {
+    var profits = [15, 20, 50],
+      weights = [1, 2, 3];
+    assert.equal(maxProfitFittingCapacityWithAnyItems(profits, weights, 5), 80);
+    assert.equal(
+      maxProfitFittingCapacityWithAnyItems(profits, weights, 6),
+      100
+    );
+  });
+  it("should return all possible coin changes of an amount with given denominations", function () {
+    var denominations = [1, 2, 3];
+    assert.equal(allPossibleChangesOf(6, denominations), 7);
+    assert.equal(allPossibleChangesOf(5, denominations), 5);
+    assert.equal(allPossibleChangesOf(7, denominations), 8);
+  });
+  it("should return the minimum denominations to change an amount with a given denominations", function () {
+    var denominations = [1, 2, 3];
+    assert.equal(minDenominationsToChange(5, denominations), 2);
+    assert.equal(minDenominationsToChange(7, denominations), 3);
+    assert.equal(minDenominationsToChange(11, denominations), 4);
+  });
+  it("should return the maximum ribbon cut given a ribbon length an cut lengths", function () {
+    var cutLengths = [2, 3, 5];
+    assert.equal(maximumRibbonCuts(5, cutLengths), 2);
+    cutLengths = [2, 3];
+    assert.equal(maximumRibbonCuts(7, cutLengths), 3);
+    cutLengths = [3, 5, 7];
+    assert.equal(maximumRibbonCuts(13, cutLengths), 3);
+  });
+});
 describe("subsets problems", function () {
-    it("should check if a set of numbers can have two partitions of equal sum", function () {
-        var array = [1, 2, 3, 4]
-        assert.isTrue(array.hasTwoPartitionsOfEqualSum())
-        array = [1, 1, 3, 4, 7]
-        assert.isTrue(array.hasTwoPartitionsOfEqualSum())
-        array = [2, 3, 4, 6]
-        assert.isFalse(array.hasTwoPartitionsOfEqualSum())
-        array = [1,  5, 7,9]
-        assert.isFalse(array.hasTwoPartitionsOfEqualSum())
-    })
-    it("should check if a subset exists with a given sum", function () {
-        var array = [1, 2, 3, 7]
-        assert.isTrue(array.hasSubsetWithSum(6))
-        array=[1,2,7,1,5]
-        assert.isTrue(array.hasSubsetWithSum(10))
-        array=[1,3,4,8]
-        assert.isFalse(array.hasSubsetWithSum(6))
-    })
-    it("should return the minimum difference between two subsets of a given array of numbers", function () {
-        var array = [1, 2, 3, 9]
-        assert.equal(array.minimumDifferenceBetweenTwoSubSetSum(), 3)
-        array=[1,2,7,1,5]
-        assert.equal(array.minimumDifferenceBetweenTwoSubSetSum(), 0)
-        array=[1,3,100,4]
-        assert.equal(array.minimumDifferenceBetweenTwoSubSetSum(), 92)
-    })
-    it("should return the count of subset with a given sum", function () {
-        var array = [1, 1, 2, 3]
-        assert.equal(array.countOfSubsetWithSum(4), 3)
-        array=[1,2,7,1,5]
-        assert.equal(array.countOfSubsetWithSum(9), 3)
-        array=[1,2,7,1,5]
-        assert.equal(array.countOfSubsetWithSum(17), 0)
-    })
-    it("should return the number of ways we can add or remove numbers in a given set to have a target sum", function () {
-        var array = [1, 1, 2, 3]
-        assert.equal(array.numberOfSymbolsCombinationToHaveTheSum(1),3) //+1-1-2+3 & -1+1-2+3 & +1+1+2-3 
-        array =[1,2,7,1]
-        assert.equal(array.numberOfSymbolsCombinationToHaveTheSum(9),2) //+1+2+7-1 & -1+2+7+1 
-    })
-})
+  it("should check if a set of numbers can have two partitions of equal sum", function () {
+    var array = [1, 2, 3, 4];
+    assert.isTrue(array.hasTwoPartitionsOfEqualSum());
+    array = [1, 1, 3, 4, 7];
+    assert.isTrue(array.hasTwoPartitionsOfEqualSum());
+    array = [2, 3, 4, 6];
+    assert.isFalse(array.hasTwoPartitionsOfEqualSum());
+    array = [1, 5, 7, 9];
+    assert.isFalse(array.hasTwoPartitionsOfEqualSum());
+  });
+  it("should check if a subset exists with a given sum", function () {
+    var array = [1, 2, 3, 7];
+    assert.isTrue(array.hasSubsetWithSum(6));
+    array = [1, 2, 7, 1, 5];
+    assert.isTrue(array.hasSubsetWithSum(10));
+    array = [1, 3, 4, 8];
+    assert.isFalse(array.hasSubsetWithSum(6));
+  });
+  it("should return the minimum difference between two subsets of a given array of numbers", function () {
+    var array = [1, 2, 3, 9];
+    assert.equal(array.minimumDifferenceBetweenTwoSubSetSum(), 3);
+    array = [1, 2, 7, 1, 5];
+    assert.equal(array.minimumDifferenceBetweenTwoSubSetSum(), 0);
+    array = [1, 3, 100, 4];
+    assert.equal(array.minimumDifferenceBetweenTwoSubSetSum(), 92);
+  });
+  it("should return the count of subset with a given sum", function () {
+    var array = [1, 1, 2, 3];
+    assert.equal(array.countOfSubsetWithSum(4), 3);
+    array = [1, 2, 7, 1, 5];
+    assert.equal(array.countOfSubsetWithSum(9), 3);
+    array = [1, 2, 7, 1, 5];
+    assert.equal(array.countOfSubsetWithSum(17), 0);
+  });
+  it("should return the number of ways we can add or remove numbers in a given set to have a target sum", function () {
+    var array = [1, 1, 2, 3];
+    assert.equal(array.numberOfSymbolsCombinationToHaveTheSum(1), 3); //+1-1-2+3 & -1+1-2+3 & +1+1+2-3
+    array = [1, 2, 7, 1];
+    assert.equal(array.numberOfSymbolsCombinationToHaveTheSum(9), 2); //+1+2+7-1 & -1+2+7+1
+  });
+});
